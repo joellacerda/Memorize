1fe07b9bc3153e3e7453a735adf847e354814832
Joel Lacerda <joellacerdaol@gmail.com>
2023-10-19 10:50:14 -0300

    add CardView

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -5,4 +5,31 @@
 //  Created by Joel Lacerda on 19/10/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.white)
+            RoundedRectangle(cornerRadius: 12)
+                .strokeBorder(lineWidth: 2)
+                .foregroundStyle(.orange)
+            Text("👻")
+                .font(.largeTitle)
+            if isFaceUp == false {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundStyle(.orange)
+                Image(systemName: "brain.filled.head.profile")
+                    .font(.largeTitle)
+            }
+        }
+    }
+}
+
+#Preview {
+    CardView()
+        .padding()
+}
--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
     }
